6999a034382c93f4853d7f0a29176f6d6125c6f9
Chris <christopher-leonardo@outlook.de>
2026-08-18 01:16:06 +0200

    ehm still trying to work on mental model of how I want to do this
    My C not very good

--- multiNeuronXor.c
+++ multiNeuronXor.c
@@ -3,11 +3,26 @@
 #include <stdlib.h>
 #include <time.h>
 
+float xor_data[][3] = {
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 0},
+    {0, 0, 0},
+};
+
 typedef struct {
-  float *weihts;
+  float w1;
+  float w2;
   float bias;
   float output;
 } Neuron;
+
+float err(Neuron *neurons) {
+  float err = 0;
+  for (int i = 0; i < sizeof(xor_data) / sizeof(xor_data[0]); i++) {
+  }
+  return 0.0f;
+}
 
 int main() {
   Neuron neuron1;
